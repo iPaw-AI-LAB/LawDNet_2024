194634ebaf0bc832bd10ddb5da33d2ed1d3bc285
dengjunliok <dengjunliok@cuc.edu.cn>
2024-03-19 15:56:51 +0000

    save -1

--- config/experiment/config_experiment_frame_256.py
+++ config/experiment/config_experiment_frame_256.py
@@ -5,8 +5,8 @@ experiment_config = {
     'mouth_region_size': 256,
     'batch_size': 8,
     'coarse2fine': True,  # 从预训练模型开始训练
-    'coarse_model_path': './output/training_model_weight/frame_training_128/netG_model_epoch_4.pth',# 会自动根据实验名字添加子文件夹
+    'coarse_model_path': './output/training_model_weight/frame_training_128/netG_model_epoch_1.pth',# 会自动根据实验名字添加子文件夹
     'result_path': './output/training_model_weight/frame_training_256', # 会自动根据实验名字添加子文件夹
-    'non_decay': 2,
-    'decay': 2
+    'non_decay': 1,
+    'decay': 1
 }
--- config/experiment/config_experiment_frame_64.py
+++ config/experiment/config_experiment_frame_64.py
@@ -6,6 +6,6 @@ experiment_config = {
     'batch_size': 4,
     'coarse2fine': False,  # 从随机初始化开始训练
     'result_path': './output/training_model_weight/frame_training_64', # 会自动根据实验名字添加子文件夹
-    'non_decay': 2,
-    'decay': 2
+    'non_decay': 1,
+    'decay': 1
 }
